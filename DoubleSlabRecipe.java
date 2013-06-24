a5e764a8dffd75ca0cdde8142ea7b1c049da5f0a
anaso <anyan_anaso@hotmail.co.jp>
2013-06-24 13:22:01 +0900

    add StoneSlabRecipe

---
+++ DoubleSlabRecipe.java
@@ -0,0 +1,33 @@
+package anaso.OtherMods;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.ItemStack;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
+import cpw.mods.fml.common.registry.GameRegistry;
+
+@Mod
+(
+	modid = "DoubleSlabRecipe",
+	name = "DoubleSlabRecipe",
+	version = "1.5.2"
+)
+
+@NetworkMod
+(
+		clientSideRequired = true
+)
+
+public class DoubleSlabRecipe
+{
+	@Mod.Init
+	public void ModInit(FMLInitializationEvent event)
+	{
+		GameRegistry.addRecipe(
+			new ItemStack(Block.stoneDoubleSlab, 1),
+			new Object[]{"X","X",Character.valueOf('X'),Block.stoneSingleSlab}
+		);
+	}
+}
